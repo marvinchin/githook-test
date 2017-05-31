f598b5daa7a86b3adb4815b88c38d86c8bb25d33
Marvin Chin <marvin.chin@thecarousell.com>
2017-05-31 11:15:00 +0800

    fixed some stuff

--- generate_change.js
+++ generate_change.js
@@ -1,11 +1,17 @@
-const request = require("superagent");
+const GitHub = require("github-api");
 const exec = require("child_process").exec;
+
+const gh = new GitHub({
+  token: process.env.GITHUB_API_TOKEN,
+  //token: '00a1918554b19ebeb396328ba261bc4b65cf2188',
+});
 
 function getChangeCommits() {
   return new Promise((resolve, reject) => {
-    const { TRAVIS_COMMIT_RANGE } = process.env;
+    const commitRange = process.env.TRAVIS_COMMIT_RANGE;
+    //const commitRange = '719ce850...2ac24e09d';
     // Prefixed with * to for markdown list
-    const command = `git log --pretty="format:* %s [%h by %cn]" ${TRAVIS_COMMIT_RANGE}`;
+    const command = `git log --pretty="format:* %s [%h by %cn]" ${commitRange}`;
     exec(command, (error, stdout, stderr) => {
       if (error) {
         reject(stderr);
@@ -18,35 +24,25 @@ function getChangeCommits() {
 }
 
 function postReleaseDraft (commitLog) {
-  return new Promise((resolve, reject) => {
-    const { TRAVIS_REPO_SLUG } = process.env;
-    const url = `https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases`;
-    const username = "marvinchin";
-    const password = "flymagpi3";
-    const body = `## Changelog\n${commitLog}`;
-    const data = {
-      tag_name: "latest_draft",
-      body,
-      draft: true,
-    };
-    request
-      .post(url)
-      .set("Content-Type", "application/json")
-      .auth(username, password)
-      .send(data)
-      .end((err, res) => {
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve(res.body);
-        }
-      })
-  });
+  `const repoSlug = PROCESS.env.TRAVIS_REPO_SLUG;`
+  repoSlug = 'marvinchin/githook-test'
+  const repo = gh.getRepo(repoSlug);
+  const body = `## Changelog\n${commitLog}`;
+  const opts = {
+    tag_name: "autochangelog",
+    body,
+    draft: true,
+  }
+  return repo.createRelease(opts);
 }
 
-const { TRAVIS_EVENT_TYPE, TRAVIS_BRANCH } = process.env;
-if ( TRAVIS_EVENT_TYPE == "push" && TRAVIS_BRANCH == "master") {
+const eventType = process.env.TRAVIS_EVENT_TYPE;
+const branch = process.env.TRAVIS_BRANCH;
+
+//const eventType = "push";
+//const branch = "master";
+
+if ( eventType == "push" && branch == "master") {
   getChangeCommits()
   .then(postReleaseDraft)
   .catch((err) => console.log(err));
